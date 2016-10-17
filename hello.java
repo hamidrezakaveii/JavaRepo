202b7d7b888746a4c1b9f5cbe0d398614880d68b
Hamidreza <hamidreza.kaveii@gmail.com>
2016-10-17 15:28:04 -0400

    test x for greater than 4

---
+++ hello.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author cstuser
+ */
+public class hello {
+    public static void main (String[] args){
+        System.out.println("hello world!");
+        int x = 5;
+        if (x > 4){
+            System.out.println("x grather than 4");
+        }
+    }
+}
