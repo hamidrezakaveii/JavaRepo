920d1e37ff6c0ac1fa4b7cd4ec017678bd839b09
hamidrezakaveii <hamidreza.kaveii@gmail.com>
2016-10-24 15:01:30 -0400

    more changes

--- JavaApplication1.java
+++ JavaApplication1.java
@@ -19,6 +19,8 @@ public class JavaApplication1 {
         int x = 5;
         if (x > 4){
             System.out.println("x grather than 4");
+			int age;
+			System.out.println("Age in month is "+age*12);
         //comment edition for test
 		// 1- ask user for his age
 		// 2- Calculate the number of months
